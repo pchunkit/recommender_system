92174b7f1299eeeeea083504adea036bdbdc8135
Jiaqi0602 <jiaqi0602@gmail.com>
2022-01-13 09:34:32 +0800

    add codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import pandas as pd
 import os 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-<<<<<<< HEAD
-=======
-from rotten_tomatoes_scraper.rt_scraper import MovieScraper
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
 
 rating = pd.io.parsers.read_csv('ml-1m/ratings.dat', 
                                 names=['user_id', 'movie_id', 'rating', 'time'],
@@ -15,18 +11,11 @@ rating = pd.io.parsers.read_csv('ml-1m/ratings.dat',
 movies = pd.io.parsers.read_csv('ml-1m/movies.dat',
                                 names=['movie_id', 'title', 'genre'],
                                 engine='python', delimiter='::')
-<<<<<<< HEAD
-=======
-
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
 users = pd.io.parsers.read_csv('ml-1m/users.dat',
                                 names=['user_id', 'gender', 'age', 'occupation', 'zip_code'],
                                 engine='python', delimiter='::')
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
 # from wordcloud import WordCloud
 # import matplotlib.pyplot as plt 
 # def plot_wordcloud(): 
@@ -37,10 +26,6 @@ users = pd.io.parsers.read_csv('ml-1m/users.dat',
 #     fig.tight_layout(pad=-1)
 #     return st.pyplot(fig)
     
-<<<<<<< HEAD
-=======
-
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
 class content_based_filtering:
     def __init__(self): 
         tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
@@ -78,17 +63,10 @@ class collaborative_based_filtering:
         top_indexes = sort_indexes[:top_n]
         top_similarity = [similarity[index] for index in top_indexes]
         recommended_movies = [movies.loc[movies["movie_id"]==(i + 1) , "title"].values[0] for i in top_indexes]
-<<<<<<< HEAD
         return recommended_movies, [(i+1) for i in top_indexes], top_similarity
             
     def calc(self, movie_title): 
         top_n = 20 
-=======
-        return recommended_movies, top_similarity
-            
-    def calc(self, movie_title): 
-        top_n = 10 
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
         k = 50 
         sliced = self.svd_matrix.T[:, :k] 
         movie_id = movies.loc[movies["title"]==movie_title, "movie_id"].values[0]
@@ -99,11 +77,7 @@ class collaborative_based_filtering:
 
 if __name__ == "__main__": 
 #     st.set_page_config(layout="wide")
-<<<<<<< HEAD
 #     content_recommender = content_based_filtering()
-=======
-    content_recommender = content_based_filtering()
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
     collab_recommender = collaborative_based_filtering()
     # streamlit apps
     st.title('Movie Recommender System')
@@ -112,7 +86,6 @@ if __name__ == "__main__":
                                 list(movies['title'].unique()))
     st.write("Selected movie: ", movie_option)
     st.write("Genre of {}: {}".format(movie_option, movies.loc[movies['title']==movie_option, 'genre'].item()))
-<<<<<<< HEAD
 #     recommended_ls = [] 
     
 #     st.write("Collaborative-based Filtering")
@@ -133,39 +106,5 @@ if __name__ == "__main__":
             movie_info = "Genre: \n{} \n\nAverage Rating: {:.1f}".format("\n".join([g for g in genre]), avg_rating)
             
             cols[idx].subheader(m_name)
-=======
-    recommended_ls = [] 
-#     st.write("Collaborative-based Filtering")
-    recommended_movies, similarity_score = collab_recommender.calc(movie_option)
-#     st.write(recommended_movies)
-    recommended_ls.extend(recommended_movies)
-#     st.write("Content-based Filtering")
-    recommended_movies, similarity_score = content_recommender.calc(movie_option)
-#     st.write(recommended_movies)
-    recommended_ls.extend(recommended_movies)
-    recommended_ls = list(set(recommended_ls))
-    ncol = 4 
-    for i in range(int(len(recommended_ls)/ncol)):
-        cols = st.columns(ncol)
-        for idx, movie_name in enumerate(recommended_ls[i*ncol: (i*ncol)+4]):
-#             cols[idx].write(movie_name)
-            try: 
-                movie_scraper = MovieScraper(movie_title=movie_name[:-6])
-                movie_scraper.extract_metadata()
-                meta_data = movie_scraper.metadata
-            except: 
-                meta_data = {}
-            
-            if len(meta_data)==4:
-                movie_info = "Rotten Score: {} | Audience Score: {} | MPAA film rating: {}".format(meta_data["Score_Audience"], meta_data['Score_Audience'], meta_data['Rating'])
-                
-            elif len(meta_data)==2: 
-                movie_info = " Rotten Score: {} | Audience Score: {}".format(meta_data["Score_Audience"], meta_data['Score_Audience'])
-                
-            else: 
-                movie_info = "Metadata not found.".format(movie_name)
-            
-            cols[idx].subheader(movie_name)
->>>>>>> 06cb91627a65deb4f692c00206a67373afccdd32
             cols[idx].markdown(movie_info)
             
